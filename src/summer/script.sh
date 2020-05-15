b99cc44fc22c74c0298c23d7b69efaabcb75eaf6
Mohammadreza Moghimi <mimr.moghimi@gmail.com>
2020-05-15 14:41:01 +0430

    end of the summer section of project

---
+++ src/summer/script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+scss ./beach-sands.scss ./beach-sands.css;
+scss ./blue-balls.scss ./blue-balls.css;
+scss ./icecream.scss ./icecream.css;
+scss ./muti-sunflower.scss ./muti-sunflower.css;
+scss ./red-fish.scss ./red-fish.css;
+scss ./single-sunflower.scss ./single-sunflower.css;
+scss ./summer-forrest.scss ./summer-forrest.css;
+scss ./sunset-balls.scss ./sunset-balls.css;
+scss ./windmill.scss ./windmill.css;
