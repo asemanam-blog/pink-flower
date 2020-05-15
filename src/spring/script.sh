0948dc6b703bb46c71b2ea6dfdbe0039c2c2f260
Mohammadreza Moghimi <mimr.moghimi@gmail.com>
2020-05-15 07:40:22 +0430

    add scripts for automation

---
+++ src/spring/script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+scss ./blossom.scss ./blossom.css;
+scss ./blue-flower.scss ./blue-flower.css;
+scss ./green-tree.scss ./green-tree.css;
+scss ./pink-tree.scss ./pink-tree.css;
+scss ./purple-flower.scss ./purple-flower.css;
+scss ./red-flower.scss ./red-flower.css;
+scss ./spring-city.scss ./spring-city.css;
+scss ./white-flower.scss ./white-flower.css;
