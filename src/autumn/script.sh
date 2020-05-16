e2781db68b4d9917ba3a7c6cb56cab375ed9f649
Mohammadreza Moghimi <mimr.moghimi@gmail.com>
2020-05-16 07:06:22 +0430

    end of the winter season

---
+++ src/autumn/script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+scss ./alone-tree.scss ./alone-tree.css;
+scss ./beauty-tree.scss ./beauty-tree.css;
+scss ./blue-sky.scss ./blue-sky.css;
+scss ./dreamy-home.scss ./dreamy-home.css;
+scss ./fall-morning.scss ./fall-morning.css;
+scss ./fall-sidewalk.scss ./fall-sidewalk.css;
+scss ./fall-way.scss ./fall-way.css;
+scss ./orange-leaf.scss ./orange-leaf.css;
+scss ./sunlight-branch.scss ./sunlight-branch.css;
+scss ./to-the-forrest.scss ./to-the-forrest.css;
+scss ./to-the-sky.scss ./to-the-sky.css;
+scss ./waterfall.scss ./waterfall.css;
+scss ./yellow-road.scss ./yellow-road.css;
---
+++ src/winter/script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+scss ./always-green.scss ./always-green.css;
+scss ./arura.scss ./arura.css;
+scss ./still-running.scss ./still-running.css;
+scss ./winter-cloth.scss ./winter-cloth.css;
+scss ./winter-home.scss ./winter-home.css;
+scss ./winter-morning.scss ./winter-morning.css;
+scss ./winter-protection.scss ./winter-protection.css;
+scss ./winter-sunrise.scss ./winter-sunrise.css;
+scss ./wooden-home.scss ./wooden-home.css;
