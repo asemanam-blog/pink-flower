6942a9aa114e56c2b5360d7c79893f860e8db8e6
Mohammadreza Moghimi <mimr.moghimi@gmail.com>
2020-05-17 01:46:11 +0430

    add shell script to compile all css in src directory

---
+++ script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd ./src/autumn; ./script.sh; cd ../..;
+cd ./src/special; ./script.sh; cd ../..;
+cd ./src/spring; ./script.sh; cd ../..;
+cd ./src/summer; ./script.sh; cd ../..;
+cd ./src/winter; ./script.sh; cd ../..;
