72a5557396e4d042b2bcb1304611e5f535024987
Mohammadreza Moghimi <mimr.moghimi@gmail.com>
2020-05-18 23:49:07 +0430

    change audio and video tag style

--- src/special/script.sh
+++ src/special/script.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-scss ./taylor1.scss ./taylor1.css;
-scss ./taylor2.scss ./taylor2.css;
-scss ./taylor3.scss ./taylor3.css;
-scss ./taylor4.scss ./taylor4.css;
-scss ./taylor5.scss ./taylor5.css;
+cd ./taylor; ./script.sh; cd ..;
