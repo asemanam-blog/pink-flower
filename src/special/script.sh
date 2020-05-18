a31af4d7883f68fe03a0f2d255d9da4bd4c8c671
Mohammadreza Moghimi <mimr.moghimi@gmail.com>
2020-05-18 23:50:38 +0430

    create new folder for yas special themes

--- src/special/script.sh
+++ src/special/script.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-cd ./taylor; ./script.sh; cd ..;
+scss ./taylor1.scss ./taylor1.css;
+scss ./taylor2.scss ./taylor2.css;
+scss ./taylor3.scss ./taylor3.css;
+scss ./taylor4.scss ./taylor4.css;
+scss ./taylor5.scss ./taylor5.css;
