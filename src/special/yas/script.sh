64321a4542383b126869fcba59be6122838f2d3d
Mohammadreza Moghimi <mimr.moghimi@gmail.com>
2020-05-20 14:47:00 +0430

    create shell script for yas special theme sass compile automation

---
+++ src/special/yas/script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+scss ./yas1.scss ./yas1.css;
+scss ./yas2.scss ./yas2.css;
+scss ./yas3.scss ./yas3.css;
+scss ./yas4.scss ./yas4.css;
+scss ./yas5.scss ./yas5.css;
+scss ./yas6.scss ./yas6.css;
+scss ./yas8.scss ./yas8.css;
+scss ./yas9.scss ./yas9.css;
+scss ./yas10.scss ./yas10.css;
+scss ./yas11.scss ./yas11.css;
+scss ./yas12.scss ./yas12.css;
+scss ./yas13.scss ./yas13.css;
+scss ./yas14.scss ./yas14.css;
+scss ./yas15.scss ./yas15.css;
+scss ./yas16.scss ./yas16.css;
